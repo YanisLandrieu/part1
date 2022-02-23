5c1169b16d24ee96b89b4560613448b639c957ed
YanisLandrieu <91880045+YanisLandrieu@users.noreply.github.com>
2022-02-23 13:01:48 +0100

    var dump + design

--- index.php
+++ index.php
@@ -1,25 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php 
 
+require_once('./controllers/index-controller.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- css -->
     <link rel="stylesheet" href="./assets/style.css">
+    <!-- js -->
     <script src="./assets/script.js"></script>
+    <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>allPIX</title>
 </head>
 
 <body>
-    <<div class="wrapper">
+    <div class="wrapper">
         <div class="text-center mt-4 name"> allPIX </div>
-        <form class="p-3 mt-3">
+        <form method="POST" class="p-3 mt-3" action="">
             <label for="">Login</label>
             <div class="form-field d-flex align-items-center"> <span class="far fa-user"></span> <input type="text" name="userName" id="userName" placeholder=""> </div>
             <label for="">Password</label>
             <div class="form-field d-flex align-items-center"> <span class="fas fa-key"></span> <input type="password" name="password" id="pwd" placeholder=""> </div><br>
-            <button class="btn mt-3">Connexion</button>
+            <button type="submit" class="btn mt-3">Connexion</button>
         </form>
         </div>
 </body>
