6eceaa341532ccb1b62b48841c2d60dbc58e28d5
YanisLandrieu <91880045+YanisLandrieu@users.noreply.github.com>
2022-02-23 12:11:36 +0100

    part2 index

--- index.php
+++ index.php
@@ -1,57 +1,27 @@
-<?php
-
-if (isset($_FILES['fileToUpload']) && $_FILES['fileToUpload']['error'] == 0) {
-    /* rename("./img/" . $_FILES['fileToUpload']['name'], "./img/" . uniqid() . ".PNG"); */
-    $tmp = $_FILES['fileToUpload']['tmp_name'];
-    $filename = $_FILES['fileToUpload']['name'];
-    // droit min sur le dossier img 733 (rwx-wx-wx)
-    $dest = 'img/';
-    $extensions_valides = array('jpg', 'jpeg', 'png');
-    $filename = strtolower($filename);
-    $fileExtension = explode(".", $filename);
-    if (in_array($fileExtension, $extensions_valides)) {
-        echo   $erreurbis = 'Upload effectué avec succès !';
-    } else {
-        echo  $erreurbis = 'Extension incorrecte. Vous ne pouvez utiliser comme image uniquement des fichiers jpg ou png';
-    }
-}
-
-?>
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- css -->
     <link rel="stylesheet" href="./assets/style.css">
-    <!-- js -->
-    <script src="./assets/script.js" async></script>
-    <!-- bootstrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Modules d'enregistrement d'image</title>
+    <script src="./assets/script.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>allPIX</title>
 </head>
 
 <body>
-    <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-            <h1 class="display-4">Module d'enregistrement d'images.</h1>
-            <p class="lead">Mise en pratique PHP : Upload d'images </p>
+    <<div class="wrapper">
+        <div class="text-center mt-4 name"> allPIX </div>
+        <form class="p-3 mt-3">
+            <label for="">Login</label>
+            <div class="form-field d-flex align-items-center"> <span class="far fa-user"></span> <input type="text" name="userName" id="userName" placeholder=""> </div>
+            <label for="">Password</label>
+            <div class="form-field d-flex align-items-center"> <span class="fas fa-key"></span> <input type="password" name="password" id="pwd" placeholder=""> </div><br>
+            <button class="btn mt-3">Connexion</button>
+        </form>
         </div>
-    </div>
-    <div style="margin-left: 15px;">
-        <?php // à faire lorsque que "tous" les risquesont été évalués
-            if (move_uploaded_file($tmp, $dest . uniqid() . ".PNG"))
-                echo 'téléchargement réussi'; 
-        ?>
-    </div>
-    <label style="margin-left: 15px;" for="fileToUpload">Veuillez choisir une image :</label>
-    <img id="imgPreview">
-    <form action="" method="post" enctype="multipart/form-data" style="margin-left: 15px;">
-        <input type="file" name="fileToUpload" id="fileToUpload"><br>
-        <br><button type="submit" style="background-color: black; color:white" class="btn btn-secondary btn-sm">Upload</button>
-    </form>
 </body>
 
 </html>
---
+++ index2.php
@@ -0,0 +1,57 @@
+<?php
+
+if (isset($_FILES['fileToUpload']) && $_FILES['fileToUpload']['error'] == 0) {
+    /* rename("./img/" . $_FILES['fileToUpload']['name'], "./img/" . uniqid() . ".PNG"); */
+    $tmp = $_FILES['fileToUpload']['tmp_name'];
+    $filename = $_FILES['fileToUpload']['name'];
+    // droit min sur le dossier img 733 (rwx-wx-wx)
+    $dest = 'img/';
+    $extensions_valides = array('jpg', 'jpeg', 'png');
+    $filename = strtolower($filename);
+    $fileExtension = explode(".", $filename);
+    if (in_array($fileExtension, $extensions_valides)) {
+        echo   $erreurbis = 'Upload effectué avec succès !';
+    } else {
+        echo  $erreurbis = 'Extension incorrecte. Vous ne pouvez utiliser comme image uniquement des fichiers jpg ou png';
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- css -->
+    <link rel="stylesheet" href="./assets/style.css">
+    <!-- js -->
+    <script src="./assets/script.js" async></script>
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Modules d'enregistrement d'image</title>
+</head>
+
+<body>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Module d'enregistrement d'images.</h1>
+            <p class="lead">Mise en pratique PHP : Upload d'images </p>
+        </div>
+    </div>
+    <div style="margin-left: 15px;">
+        <?php // à faire lorsque que "tous" les risquesont été évalués
+            if (move_uploaded_file($tmp, $dest . uniqid() . ".PNG"))
+                echo 'téléchargement réussi'; 
+        ?>
+    </div>
+    <label style="margin-left: 15px;" for="fileToUpload">Veuillez choisir une image :</label>
+    <img id="imgPreview">
+    <form action="" method="post" enctype="multipart/form-data" style="margin-left: 15px;">
+        <input type="file" name="fileToUpload" id="fileToUpload"><br>
+        <br><button type="submit" style="background-color: black; color:white" class="btn btn-secondary btn-sm">Upload</button>
+    </form>
+</body>
+
+</html>
