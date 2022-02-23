2bea9b9cad3645414a0c4d317a89c5e4386f86a0
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2022-02-23 13:31:55 +0100

    index-controller

--- controllers/index-controller.php
+++ controllers/index-controller.php
@@ -1,5 +1,15 @@
-<?php // Vérification des données de login
+<?php 
+    include_once 'my-config.php';
+
+    // Vérification des données de login
 
     var_dump($_POST);
+    var_dump($informationLogin);
 
+    // if(!($_POST == $informationLogin[0])){
+    //     echo "mauvais login";
+    // }
+    // if(!($_POST == $informationLogin[1])){
+    //        echo "mauvais mdp";
+    // }
 ?>
