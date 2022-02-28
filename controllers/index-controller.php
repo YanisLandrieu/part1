9abde8635bf046ead98bd27b708830f495411a96
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2022-02-28 10:25:35 +0100

    move css + correct & incorrect login/mdp

--- controllers/index-controller.php
+++ controllers/index-controller.php
@@ -5,24 +5,13 @@
 
     // var_dump($_POST);
     // var_dump($informationLogin);
-    echo $informationLogin['mdp'];
+    // echo $informationLogin['mdp'];
 
     // function verifyLogin() {
         if((isset($_POST['login'])) && (isset($_POST['password']))) {
             if(($_POST['login'] == $informationLogin['login']) && ($_POST['password'] == $informationLogin['mdp'])){
                 echo 'correct';
             }
-            else {
-                if(!($_POST['password'] == $informationLogin['mdp'])){
-                    echo 'password incorrect';
-                }
-                if(!($_POST['login'] == $informationLogin['login'])){
-                    echo 'login incorrect';
-                }
-            }   
-        }
-        else {
-            echo 'fail 1';
         }
     // }
 ?>
