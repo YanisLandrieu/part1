e09c837c25bd40adb6e38913e82f2f4d211f9de7
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2022-03-01 11:14:25 +0100

    variable dashboard

---
+++ controllers/dashboard-controller.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+/**
+ * Fonction qui vérifie l'image envoyer par l'utilisateur
+ *
+ * @return void
+ */
+var_dump(getFileSize());
+function uploadImg() {
+    include('../my-config.php');
+    $imgError = 'Tout va bien';
+    // On vérifie si $_FILES est définie ou si il n'y a pas d'erreur
+    if (isset($_FILES['fileToUpload']) && $_FILES['fileToUpload']['error'] == 0) {
+        $tmp = $_FILES['fileToUpload']['tmp_name'];
+        $filename = $_FILES['fileToUpload']['name'];
+        
+        $dest = '../img/';
+        $extensions_valides = array('jpg', 'jpeg', 'png');
+        $filename = strtolower($filename);
+        $fileExtension = explode(".", $filename);
+        // On vérifie si la taille de l'image est plus lourde que l'espace restant
+        if(!(getFileSize() < 50000 - $informationLogin['quota'])){
+            $imgError = 'l\'image est trop lourde';
+            require_once("../views/dashbord.php");
+        }
+        else{
+            // On vérifie si l'extension de l'image est valide
+            if (in_array($fileExtension[1], $extensions_valides)) {
+                $imgError = 'Upload effectué avec succès !';
+                if (move_uploaded_file($tmp, $dest . uniqid() . ".png"))
+                    // On ajoute la taille de l'image au quota
+                    $informationLogin['quota'] = $informationLogin['quota'] + getFileSize();
+                    // On ajoute 1 au compteur d'image
+                    $informationLogin['formule']= $informationLogin['formule'] + 1;
+                    require_once("../views/dashbord.php");
+            } // Sinon on renvoie un message d'erreur
+            else {
+                $imgError = 'Extension incorrecte. Vous ne pouvez utiliser comme image uniquement des fichiers jpg, jpeg ou png';
+                require_once("../views/dashbord.php");
+            }
+        }
+    } // Sinon on ne fait rien
+    else{
+        require_once("../views/dashbord.php");
+    }
+}
+/**
+ * Récupère la taille d'une image
+ *
+ * @return int
+ */
+
+function getFileSize() {
+    return $_FILES['fileToUpload']['size'];
+}
+
+uploadImg();
+?>
--- views/dashbord.php
+++ views/dashbord.php
@@ -1,40 +1,5 @@
 <?php 
 include_once('../my-config.php');
-session_start();
-
-/**
- * Fonction qui vérifie l'image envoyer par l'utilisateur
- *
- * @return void
- */
-function uploadImg() {
-    include_once('../my-config.php');
-    if (isset($_FILES['fileToUpload']) && $_FILES['fileToUpload']['error'] == 0) {
-        $tmp = $_FILES['fileToUpload']['tmp_name'];
-        $filename = $_FILES['fileToUpload']['name'];
-        // droit min sur le dossier img 733 (rwx-wx-wx)
-        $dest = 'img/';
-        $extensions_valides = array('jpg', 'jpeg', 'png');
-        $filename = strtolower($filename);
-        $fileExtension = explode(".", $filename);
-        if(!(getFileSize() <= $informationLogin['quota'])){
-            echo 'l\'image est trop lourde';
-        }
-        if (in_array($fileExtension, $extensions_valides)) {
-            echo 'Upload effectué avec succès !';
-        } else {
-            echo 'Extension incorrecte. Vous ne pouvez utiliser comme image uniquement des fichiers jpg, jpeg ou png';
-        }
-    }
-
-    if (move_uploaded_file($tmp, $dest . uniqid() . ".PNG"))
-        echo 'téléchargement réussi'; 
-}
-
-function getFileSize() {
-    return $_FILES['fileToUpload']['size'];
-}
-
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -44,7 +9,7 @@ function getFileSize() {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- css -->
-    <link rel="stylesheet" href="../assets/style.css">
+    <link rel="stylesheet" href="../style.css">
     <!-- js -->
     <script src="./assets/script.js"></script>
     <!-- bootstrap -->
@@ -59,16 +24,14 @@ function getFileSize() {
             <p>Bonjour, <?= $_SESSION['username']; ?></p>
         </div>
         <div class="text-center mt-3 name">
-            <p>Quota : <?= $informationLogin['quota']; ?> / 50 Mo</p>
+            <p>Quota : <?= $informationLogin['quota']; ?> / 50000 ko</p>
             <p>Total image(s) : <?= $informationLogin['formule']; ?></p>
         </div>
-        <form action="" method="post" enctype="multipart/form-data">
-            <button type="submit" class="btn ">Upload</button>
+        <form action="http://part1.test/controllers/dashboard-controller.php" method="post" enctype="multipart/form-data">
             <input style="margin-left: 60px;" type="file" name="fileToUpload" id="fileToUpload">
+            <button type="submit" class="btn ">Upload</button>
         </form>
-        <p style="margin-left: 90px;">vos message icI</p>
-        <p style="margin-left: 90px;">- upload ok</p>
-        <p style="margin-left: 90px;">- upload ok</p>
+        <p style="margin-left: 90px;"><?=$imgError;?></p>
         <button style="margin-left: 90px;" type="submit" class="btn ">Gallery</button><br>
         <br><a style="text-decoration: none; color:white; margin-left: 105px;" class="text-center mt-4" href="http://part1.test">Déconnexion</a>
     </div>
