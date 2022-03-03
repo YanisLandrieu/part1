ab956da2f39cd58cbfc366d7c85dfcb66382037d
YanisLandrieu <91880045+YanisLandrieu@users.noreply.github.com>
2022-03-03 09:43:31 +0100

    deconnection

---
+++ views/deconnection.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- css -->
+    <link rel="stylesheet" href="../style.css">
+    <!-- js -->
+    <script src="./assets/script.js"></script>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>allPIX</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="text-center mt-4 name">
+            <p class="titre">allPix</p>
+        </div><br>
+        <div class="text-center mt-4 name">
+            <p>Vous avez bien été deconnecter, au plaisir de vous revoir ! </p>
+        </div><br>
+        <br><a style="text-decoration: none; color:white; margin-left: 120px;" class="text-center mt-4" href="http://part1.test/">Accueil</a>
+    </div>
+</body>
+
+</html>
